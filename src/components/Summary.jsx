53ff5c8566608ecaa62c3784afcbc04e27813cd8
Gorock Shetty <gorock397@gmail.com>
2023-05-29 14:53:19 +1000

    link copy feature implemented

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -4,13 +4,10 @@ import { copy, linkIcon, loader, tick } from '../assets';
 import { useLazyGetSummaryQuery } from '../services/article';
 
 const Summary = () => {
-  const [article, setArticle] = useState({
-    url:'',
-    summary:'',
-  });
+  const [article, setArticle] = useState({url:'',summary:'',});
   const [allArticles, setAllArticles] = useState([]);
-
   const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+  const [copied, setCopied] = useState('');
 
   useEffect(()=>{
     const articlesFromLocalStorage = JSON.parse(
@@ -33,6 +30,12 @@ const Summary = () => {
       
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
+  }
+
+  const handleCopy = (copyUrl) =>{
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000)
   }
 
   return (
@@ -65,11 +68,13 @@ const Summary = () => {
         <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
           {allArticles.reverse().map((item, index) => (
               <div key={`link-${index}`} onClick={()=> setArticle(item)} className='link_card'>
-                <div className='copy_btn'>
-                  <img src={copy} alt='copy_icon' className='w-[40%] h-[40%] object-contain' />
+                <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                  <img 
+                  src={copied === item.url? tick:copy}
+                  alt={copied === item.url? 'tick_icon': 'copy_icon'}
+                  className='w-[40%] h-[40%] object-contain' />
                 </div>
                 <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>{item.url}</p>
-              
               </div>
           ))}
         </div>
